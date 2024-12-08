535a25f65f85cc0b5f43570d25181395f902da87
Elman <0606elman1999@mail.ru>
2024-12-08 20:07:16 +0300

    1 попытка

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,36 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const content = document.querySelector('.content');
+const cardContent = content.querySelector('.places__list');
+
+function pushCard() {
+  initialCards.forEach(({name, link}) => {
+    cardContent.append(addCard({name, link}, deleteCard));
+  });
+};
+
+function addCard({name, link}, deleteCard){
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  
+  cardElement.querySelector('.card__title').textContent = name;
+  cardElement.querySelector('.card__image').src = link;
+
+  cardDeleteButton.addEventListener('click', () => deleteCard(cardElement));
+
+  return cardElement;
+};
+  
+function deleteCard(cardElement) {
+  cardElement.remove();
+};
+
+pushCard();
+
+
+
+
+
